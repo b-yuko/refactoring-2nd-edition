6b89a9a3d77f7eff15166516ab9ee362a2b6a983
b-yuko <yuko.banno.oo@gmail.com>
2025-01-09 08:58:15 +0900

    refactor: 関数宣言の変更

--- code/src/service/VideoRentalCalculator.ts
+++ code/src/service/VideoRentalCalculator.ts
@@ -10,11 +10,11 @@ export function statement(invoices: Invoice[], plays: Plays): string {
     volumeCredits += volumeCreditsFor(perf)
 
     // 注文の内訳を出力
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats) \n`;
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats) \n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)} \n`;
+  result += `Amount owed is ${usd(totalAmount)} \n`;
   result += `You earned ${volumeCredits} credits \n`;
 
   return result;
@@ -56,11 +56,11 @@ export function statement(invoices: Invoice[], plays: Plays): string {
     return result
   }
 
-  function format(aNumber: number){
+  function usd(aNumber: number){
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 }
